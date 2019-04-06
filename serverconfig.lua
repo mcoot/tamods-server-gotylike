3b75fc81df4e1590059d067cab1705bb9a112955
JosephSpearritt <joe.spearritt@live.com>
2019-04-06 22:08:44 +1100

    Add server info

--- serverconfig.lua
+++ serverconfig.lua
@@ -5,6 +5,12 @@ require("gotylike/main")
 
 Logger.debug("----- GOTYLike Preset Applied -----")
 
+-- Server Information
+ServerSettings.Description = "My Custom GOTY Server"
+ServerSettings.Motd = "This is my GOTY server"
+-- ServerSettings.Password = "some-password"
+ServerSettings.GameSettingMode = ServerSettings.GameSettingModes.GOTY
+
 -- Map Rotation
 -- ServerSettings.MapRotation.add(Maps.Arena.WalledIn)
 -- ServerSettings.MapRotation.add(Maps.TDM.DrydockNight)
@@ -16,3 +22,6 @@ ServerSettings.MapRotation.add(Maps.CTF.Terminus)
 ServerSettings.MapRotation.add(Maps.CTF.Sunstar)
 ServerSettings.MapRotation.add(Maps.CTF.Drydock)
 ServerSettings.MapRotation.add(Maps.CTF.BellaOmegaNS)
+
+-- Uncomment below to enable event logging
+-- Core.EventLogging = true
