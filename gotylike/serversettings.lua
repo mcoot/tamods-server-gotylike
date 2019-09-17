bb715f31d1a8cf7747525629348a3105232da395
JosephSpearritt <joe.spearritt@live.com>
2019-09-17 21:18:29 +1000

    Add InventoryCallInBlocksPlayers config

--- gotylike/serversettings.lua
+++ gotylike/serversettings.lua
@@ -26,6 +26,7 @@ ServerSettings.UseGOTYBXTCharging = true
 ServerSettings.RageThrustPackDependsOnCapperSpeed = false
 -- Enable using the laser targeter to call in Inv Stations
 ServerSettings.EnableInventoryCallIn = true
+ServerSettings.InventoryCallInBlocksPlayers = true
 ServerSettings.InventoryCallInCost = 4000
 ServerSettings.InventoryCallInBuildUpTime = 2.0
 ServerSettings.InventoryCallInCooldownTime = 10.0
@@ -51,6 +52,10 @@ ServerSettings.BeowulfEjectionSeat = false
 ServerSettings.ShrikeEjectionSeat = false
 
 -- Vehicles testing
+-- ServerSettings.InventoryCallInCost = 0
+-- ServerSettings.GravCycleCost = 0
+-- ServerSettings.BeowulfCost = 0
+-- ServerSettings.ShrikeCost = 0
 
 -- Weapon bans
 
