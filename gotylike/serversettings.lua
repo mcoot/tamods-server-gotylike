314938cdf60373d94f07b59237c42814a6563728
JosephSpearritt <joe.spearritt@live.com>
2019-09-21 14:26:05 +1000

    Refactor serversettings

--- gotylike/serversettings.lua
+++ gotylike/serversettings.lua
@@ -1,67 +1,61 @@
--- Game Rules
-ServerSettings.TimeLimit = 25
-ServerSettings.WarmupTime = 120
-ServerSettings.RespawnTime = 5
-ServerSettings.EndMatchWaitTime = 46
-ServerSettings.SniperRespawnDelay = 5
-ServerSettings.TeamAssignType = TeamAssignTypes.Unbalanced
-ServerSettings.AutoBalanceTeams = false
-ServerSettings.CTFCapLimit = 7
-ServerSettings.FriendlyFire = true
-ServerSettings.FriendlyFireMultiplier = 1
-ServerSettings.BaseAssets = false
-ServerSettings.AmmoPickupLifespan = 60
-ServerSettings.CTFFlagTimeout = 40
-ServerSettings.TeamCredits = true
-
--- Make inventory stations give energy
-ServerSettings.InventoryStationsRestoreEnergy = true
--- Disable heavy shield pack impulse reduction
-ServerSettings.UseGOTYShieldPack = true
--- Re-enable GOTY BXT charging
-ServerSettings.UseGOTYBXTCharging = true
--- Re-enable Airburst on the Jackal
--- ServerSettings.UseGOTYJackalAirburst = true
--- Disable the OOTB Rage effect causing Thrust speed to depend on the capper's grab speed
-ServerSettings.RageThrustPackDependsOnCapperSpeed = false
--- Enable using the laser targeter to call in Inv Stations
-ServerSettings.EnableInventoryCallIn = true
-ServerSettings.InventoryCallInBlocksPlayers = true
-ServerSettings.InventoryCallInCost = 4000
-ServerSettings.InventoryCallInBuildUpTime = 2.0
-ServerSettings.InventoryCallInCooldownTime = 10.0
-
--- Game Rules testing
--- ServerSettings.TimeLimit = 1
--- ServerSettings.CTFCapLimit = 1
--- ServerSettings.WarmupTime = 10
--- ServerSettings.OvertimeLimit = 0
-
--- Vehicles
-ServerSettings.VehiclesEarnedWithCredits = true
-ServerSettings.GravCycleLimit = 16
-ServerSettings.BeowulfLimit = 1
-ServerSettings.ShrikeLimit = 1
-
-ServerSettings.GravCycleCost = 5000
-ServerSettings.BeowulfCost = 25000
-ServerSettings.ShrikeCost = 40000
-
-ServerSettings.GravCycleEjectionSeat = false
-ServerSettings.BeowulfEjectionSeat = false
-ServerSettings.ShrikeEjectionSeat = false
-
--- Vehicles testing
--- ServerSettings.InventoryCallInCost = 0
--- ServerSettings.GravCycleCost = 0
--- ServerSettings.BeowulfCost = 0
--- ServerSettings.ShrikeCost = 0
-
--- Weapon bans
+Core.AllowUnmoddedClients = false
 
 -- Limit all classes to two weapons
 ServerSettings.DisabledEquipPoints.add("Light", Loadouts.EquipPoints.Tertiary)
 ServerSettings.DisabledEquipPoints.add("Medium", Loadouts.EquipPoints.Tertiary)
 ServerSettings.DisabledEquipPoints.add("Heavy", Loadouts.EquipPoints.Tertiary)
 
-Core.AllowUnmoddedClients = false
+defaultGotySettings = {
+    -- Time settings
+    TimeLimit = 25,
+    WarmupTime = 120,
+    RespawnTime = 5,
+    SniperRespawnDelay = 5,
+    EndMatchWaitTime = 45,
+    AmmoPickupLifespan = 60,
+    CTFFlagTimeout = 40,
+    -- Team settings
+    TeamAssignType = TeamAssignTypes.Unbalanced,
+    AutoBalanceTeams = false,
+    FriendlyFire = true,
+    FriendlyFireMultiplier = 1,
+    TeamCredits = true,
+    BaseAssets = false,
+    -- Score
+    CTFCapLimit = 7,
+
+    -- Vehicles
+    VehiclesEarnedWithCredits = true,
+    GravCycleLimit = 16,
+    BeowulfLimit = 1,
+    ShrikeLimit = 1,
+    GravCycleCost = 5000,
+    BeowulfCost = 25000,
+    ShrikeCost = 40000,
+    GravCycleEjectionSeat = false,
+    BeowulfEjectionSeat = false,
+    ShrikeEjectionSeat = false,
+    
+    -- Inventory call-in
+    EnableInventoryCallIn = true,
+    InventoryStationsRestoreEnergy = true,
+    InventoryCallInBlocksPlayers = true,
+    InventoryCallInCost = 4000,
+    InventoryCallInBuildUpTime = 2.0,
+    InventoryCallInCooldownTime = 10.0,
+
+    -- GOTY fixes
+    UseGOTYShieldPack = true,
+    UseGOTYBXTCharging = true,
+    RageThrustPackDependsOnCapperSpeed = false,
+}
+
+-- Function to set or override any settings
+function applyServerSettings(settings)
+    for setting, value in pairs(settings) do
+        ServerSettings[setting] = value
+    end
+end
+
+-- Apply GOTY settings at preset load time
+applyServerSettings(defaultGotySettings)
